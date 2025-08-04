9a8d61a29a7ea58367c40c8064de4c9ffb073add
JAHNAVI-YERRAMSETTI <2300031078@kluniversity.in>
2025-08-04 10:01:21 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'jenkinserp', //deployment folder name but not git repo
+
 })
